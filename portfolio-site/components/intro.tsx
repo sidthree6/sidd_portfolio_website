053dae1dc8affaaed515b448c408619c1c5f966b
sidthree16 <psiddhu001@gmail.com>
2022-10-26 11:04:43 -0400

    Escaped quote in intro

--- portfolio-site/components/intro.tsx
+++ portfolio-site/components/intro.tsx
@@ -1,7 +1,7 @@
 const Intro = () => {
     return(
         <div className="text-center">
-            <div className="font-serif text-slate-400 text-xl italic">\" Hello World \"</div>
+            <div className="font-serif text-slate-400 text-xl italic">&quot; Hello World &quot;</div>
             <div className="text-slate-500 text-sm">
                 I am an experienced Software Developer / Pipeline Technical Director who enjoys every aspects of technologies be it using rocks to create fire or cars that goes in space, and anything in-between.
                 <br/> Hands on experience with Software Engineering, Data analytics / Science, Full Stack development, Game deveopment. Various programming languages and tools knowledge such as python, javascript, php, c++, c#, unreal engine, autodesk maya, nuke, houdini, unity.
